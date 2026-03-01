812716b704b7f1ebe279ac7ac586629d9b6b7c25
Kunhua Huang <huang.kunh@northeastern.edu>
2026-03-01 04:38:38 -0500

    define openapi and proto

--- internal/order/main.go
+++ internal/order/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"io"
 	"log"
-	"net/http"
+
+	"github.com/ecstasoy/gorder/common/config"
+	"github.com/spf13/viper"
 )
 
-func main() {
-	log.Println("starting server, listening on port 8090")
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%v", r.RequestURI)
-		_, _ = io.WriteString(w, "Welcome to home page")
-	})
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%v", r.RequestURI)
-		_, _ = io.WriteString(w, "pong")
-	})
-	if err := http.ListenAndServe(":8090", mux); err != nil {
+func init() {
+	if err := config.NewViperConfig(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	log.Printf("%v", viper.Get("order"))
 }
