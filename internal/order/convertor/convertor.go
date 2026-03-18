e005cbd24c313d73f52fec5cbca04a61083d840a
Kunhua Huang <huang.kunh@northeastern.edu>
2026-03-18 06:43:23 -0400

    feat: implement order conversion utilities and singleton constructors

---
+++ internal/order/convertor/convertor.go
@@ -0,0 +1,177 @@
+package convertor
+
+import (
+	client "github.com/ecstasoy/gorder/common/client/order"
+	"github.com/ecstasoy/gorder/common/genproto/orderpb"
+	domain "github.com/ecstasoy/gorder/order/domain/order"
+	"github.com/ecstasoy/gorder/order/entity"
+)
+
+type OrderConvertor struct{}
+type ItemConvertor struct{}
+type ItemWithQuantityConvertor struct{}
+
+func (c *ItemWithQuantityConvertor) EntitiesToProtos(items []*entity.ItemWithQuantity) (res []*orderpb.ItemWithQuantity) {
+	for _, i := range items {
+		res = append(res, c.EntityToProto(i))
+	}
+	return
+}
+
+func (c *ItemWithQuantityConvertor) EntityToProto(i *entity.ItemWithQuantity) *orderpb.ItemWithQuantity {
+	return &orderpb.ItemWithQuantity{
+		ItemID:   i.ID,
+		Quantity: i.Quantity,
+	}
+}
+
+func (c *ItemWithQuantityConvertor) ProtosToEntities(items []*orderpb.ItemWithQuantity) (res []*entity.ItemWithQuantity) {
+	for _, i := range items {
+		res = append(res, c.ProtoToEntity(i))
+	}
+	return
+}
+
+func (c *ItemWithQuantityConvertor) ProtoToEntity(i *orderpb.ItemWithQuantity) *entity.ItemWithQuantity {
+	return &entity.ItemWithQuantity{
+		ID:       i.ItemID,
+		Quantity: i.Quantity,
+	}
+}
+
+func (c *ItemWithQuantityConvertor) ClientsToEntities(items []client.ItemWithQuantity) (res []*entity.ItemWithQuantity) {
+	for _, i := range items {
+		res = append(res, c.ClientToEntity(i))
+	}
+	return
+}
+
+func (c *ItemWithQuantityConvertor) ClientToEntity(i client.ItemWithQuantity) *entity.ItemWithQuantity {
+	return &entity.ItemWithQuantity{
+		ID:       i.Id,
+		Quantity: i.Quantity,
+	}
+}
+
+func (c *OrderConvertor) EntityToProto(o *domain.Order) *orderpb.Order {
+	c.check(o)
+	return &orderpb.Order{
+		ID:          o.ID,
+		CustomerID:  o.CustomerID,
+		Status:      o.Status,
+		Items:       NewItemConvertor().EntitiesToProtos(o.Items),
+		PaymentLink: o.PaymentLink,
+	}
+}
+
+func (c *OrderConvertor) ProtoToEntity(o *orderpb.Order) *domain.Order {
+	c.check(o)
+	return &domain.Order{
+		ID:          o.ID,
+		CustomerID:  o.CustomerID,
+		Status:      o.Status,
+		PaymentLink: o.PaymentLink,
+		Items:       NewItemConvertor().ProtosToEntities(o.Items),
+	}
+}
+
+func (c *OrderConvertor) ClientToEntity(o *client.Order) *domain.Order {
+	c.check(o)
+
+	status := orderpb.OrderStatus_ORDER_STATUS_UNSPECIFIED
+	if statusValue, ok := orderpb.OrderStatus_value[string(o.Status)]; ok {
+		status = orderpb.OrderStatus(statusValue)
+	}
+
+	return &domain.Order{
+		ID:          o.Id,
+		CustomerID:  o.CustomerId,
+		Status:      status,
+		PaymentLink: o.PaymentLink,
+		Items:       NewItemConvertor().ClientsToEntities(o.Items),
+	}
+}
+
+func (c *OrderConvertor) EntityToClient(o *domain.Order) *client.Order {
+	c.check(o)
+
+	statusStr := client.OrderStatus(o.Status.String())
+
+	return &client.Order{
+		Id:          o.ID,
+		CustomerId:  o.CustomerID,
+		Status:      statusStr,
+		PaymentLink: o.PaymentLink,
+		Items:       NewItemConvertor().EntitiesToClients(o.Items),
+	}
+}
+
+func (c *OrderConvertor) check(o interface{}) {
+	if o == nil {
+		panic("cannot convert nil order")
+	}
+}
+
+func (c *ItemConvertor) EntitiesToProtos(items []*entity.Item) (res []*orderpb.Item) {
+	for _, i := range items {
+		res = append(res, c.EntityToProto(i))
+	}
+	return
+}
+
+func (c *ItemConvertor) ProtosToEntities(items []*orderpb.Item) (res []*entity.Item) {
+	for _, i := range items {
+		res = append(res, c.ProtoToEntity(i))
+	}
+	return
+}
+
+func (c *ItemConvertor) ClientsToEntities(items []client.Item) (res []*entity.Item) {
+	for _, i := range items {
+		res = append(res, c.ClientToEntity(i))
+	}
+	return
+}
+
+func (c *ItemConvertor) EntitiesToClients(items []*entity.Item) (res []client.Item) {
+	for _, i := range items {
+		res = append(res, c.EntityToClient(i))
+	}
+	return
+}
+
+func (c *ItemConvertor) EntityToProto(i *entity.Item) *orderpb.Item {
+	return &orderpb.Item{
+		ID:       i.ID,
+		Name:     i.Name,
+		Quantity: i.Quantity,
+		PriceID:  i.PriceID,
+	}
+}
+
+func (c *ItemConvertor) ProtoToEntity(i *orderpb.Item) *entity.Item {
+	return &entity.Item{
+		ID:       i.ID,
+		Name:     i.Name,
+		Quantity: i.Quantity,
+		PriceID:  i.PriceID,
+	}
+}
+
+func (c *ItemConvertor) ClientToEntity(i client.Item) *entity.Item {
+	return &entity.Item{
+		ID:       *i.Id,
+		Name:     *i.Name,
+		Quantity: *i.Quantity,
+		PriceID:  *i.PriceId,
+	}
+}
+
+func (c *ItemConvertor) EntityToClient(i *entity.Item) client.Item {
+	return client.Item{
+		Id:       &i.ID,
+		Name:     &i.Name,
+		Quantity: &i.Quantity,
+		PriceId:  &i.PriceID,
+	}
+}
